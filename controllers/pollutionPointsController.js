6f2221ee36823f3c5964a25ec013c04ace6e11dc
Tom Hawes <twhawes@gmail.com>
2019-12-11 10:37:49 +0000

    all tests passing

--- controllers/pollutionPointsController.js
+++ controllers/pollutionPointsController.js
@@ -10,13 +10,11 @@ const getPollutionPoints = (req, res, next) => {
   fetchPollutionPoints()
     .then(pollutionPoints => {
       const geoJSON = geoJSONPollutionPoints(pollutionPoints);
-
       const geoJSONAndPollutionPoints = {
         type: 'FeatureCollection',
         features: geoJSON,
         pollutionPoints: pollutionPoints
       };
-      // console.log('outputObj ->', outputObj);
       res.status(200).json({ geoJSONAndPollutionPoints });
     })
     .catch(next);
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -31,19 +31,17 @@ describe('/users', () => {
           'email',
           'password',
           'current_location',
-          '__v'
+          'end_location'
         );
       });
   });
 });
 describe('/pollution-points', () => {
-
   it('returns an object', () => {
     return request(app)
       .get('/api/pollution-points')
       .expect(200)
       .then(({ body }) => {
-        // console.log(body.pollutionPoints[0].am.top_corner);
         expect(body).to.be.an('object');
       });
   });
@@ -52,7 +50,6 @@ describe('/pollution-points', () => {
       .get('/api/pollution-points')
       .expect(200)
       .then(({ body }) => {
-        console.log('TEST BODY ->', body);
         expect(body.geoJSONAndPollutionPoints.pollutionPoints).to.be.an(
           'array'
         );
@@ -87,54 +84,53 @@ describe('/pollution-points', () => {
       });
   });
 
-    it('returns an object', () => {
-        return request(app)
-            .get('/api/pollution-points')
-            .expect(200)
-            .then(({ body }) => {
-                expect(body).to.be.an('object')
-            })
-    });
-    it('returns an object with an array of pollution points', () => {
-        return request(app)
-            .get('/api/pollution-points')
-            .expect(200)
-            .then(({ body }) => {
-                expect(body.pollutionPoints).to.be.an('array')
-            })
-    });
-    it('returns an array of objects with the pollution points keys', () => {
-        return request(app)
-            .get('/api/pollution-points')
-            .expect(200)
-            .then(({ body }) => {
-                expect(body.pollutionPoints[0]).to.have.keys('pp_coordinates', 'pm', 'name', 'midday', 'id', 'am', '_id')
-            })
-    });
+  it('returns an object', () => {
+    return request(app)
+      .get('/api/pollution-points')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).to.be.an('object')
+      })
+  });
+  it('returns an object with an array of pollution points', () => {
+    return request(app)
+      .get('/api/pollution-points')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.geoJSONAndPollutionPoints.pollutionPoints).to.be.an('array')
+      })
+  });
+  it('returns an array of objects with the pollution points keys', () => {
+    return request(app)
+      .get('/api/pollution-points')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.geoJSONAndPollutionPoints.pollutionPoints[0]).to.have.keys('pp_coordinates', 'pm', 'name', 'midday', 'id', 'am', '_id')
+      })
+  });
 
 });
 describe('/users POST', () => {
-    it.only('returns an object with the new user', () => {
-        const randomName = faker.name.findName();
-        return request(app)
-            .post('/api/users')
-            .send({
-                username: `${randomName}`,
-                email: 'user@gmail.com',
-                password: 'dsfdfdfdfdfd',
-                current_location: '53.4860211, -2.2397307',
-                end_location: {
-                    lat: 0,
-                    long: 0
-                }
-            })
-            .expect(201)
-            .then(({ body }) => {
-                console.log(body)
-                expect(body).to.be.an('object');
-                expect(body.user).to.have.keys('username', 'email', 'password', 'current_location', '_id', '__v', 'end_location');
-            })
-    });
+  it('returns an object with the new user', () => {
+    const randomName = faker.name.findName();
+    return request(app)
+      .post('/api/users')
+      .send({
+        username: `${randomName}`,
+        email: 'user@gmail.com',
+        password: 'dsfdfdfdfdfd',
+        current_location: '53.4860211, -2.2397307',
+        end_location: {
+          lat: 0,
+          long: 0
+        }
+      })
+      .expect(201)
+      .then(({ body }) => {
+        expect(body).to.be.an('object');
+        expect(body.user).to.have.keys('username', 'email', 'password', 'current_location', '_id', '__v', 'end_location');
+      })
+  });
 });
 
 describe('/users PATCH', () => {
@@ -149,34 +145,35 @@ describe('/users PATCH', () => {
           'username',
           'email',
           'password',
-          'current_location'
+          'current_location',
+          'end_location'
         );
       });
   });
 });
-    it('updates the users geo-location', () => {
-        return request(app)
-            .patch('/api/users')
-            .send({
-                username: 'harry', current_location: '53.4860211, -2.2397307', end_location: {
-                    lat: 69, long: 69
-                }
-            })
-            .expect(200)
-            .then(({ body }) => {
-                expect(body.user).to.have.keys('_id', 'username', 'email', "password", "current_location", 'end_location')
-            })
-    });
+it('updates the users geo-location', () => {
+  return request(app)
+    .patch('/api/users')
+    .send({
+      username: 'harry', current_location: '53.4860211, -2.2397307', end_location: {
+        lat: 69, long: 69
+      }
+    })
+    .expect(200)
+    .then(({ body }) => {
+      expect(body.user).to.have.keys('_id', 'username', 'email', "password", "current_location", 'end_location')
+    })
 });
+
 describe('/pollution-point/:PP_id GET', () => {
-    it('gets one pollution point ', () => {
-        return request(app)
-            .get('/api/pollution-points/5dee7aee1f5e2e303b42954b')
-            .expect(200)
-            .then(({ body }) => {
-                expect(body.pollutionPoint).to.be.an('object')
-                expect(body.pollutionPoint.name).to.eql('Science Museum (Liverpool Road)')
-            })
-    });
+  it('gets one pollution point ', () => {
+    return request(app)
+      .get('/api/pollution-points/5df0c56a4870f2635ade79ff')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.pollutionPoint).to.be.an('object')
+        expect(body.pollutionPoint.name).to.eql('Lego Store (Brown Street)')
+      })
+  });
 });
 
